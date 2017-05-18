e6ea937917289a96a762dfbf8e89fd0d0dd6c119
Aliceravier <aliceravier@yahoo.co.uk>
2017-05-18 15:21:13 +0100

    added button and player animation

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
     private Rigidbody2D rb;
     public float speed;
+    private Animator anim;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
-        
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -17,10 +18,32 @@ public class Movement : MonoBehaviour {
         float moveVert = Input.GetAxisRaw("Vertical");
         Vector2 movement = new Vector2(moveHori, moveVert);
         rb.velocity = speed*movement;
-        if (moveHori == 0 && moveVert == 0)
+
+        //walking animation set
+        if (movement.x != 0 || movement.y != 0)
         {
-     
+            anim.SetBool("PlayerMoving", true);
+        }
+        else
+            anim.SetBool("PlayerMoving", false);
+
+        //rotate player
+        if (moveHori > 0)//right
+        {
+            rb.MoveRotation(90);
+        }
+        if (moveHori < 0)//left
+        {
+            rb.MoveRotation(270);
         }
 
+        if (moveVert > 0)//up
+        {
+            rb.MoveRotation(0);
+        }
+        if (moveVert < 0)//down
+        {
+            rb.MoveRotation(180);
+        }
     }
 }

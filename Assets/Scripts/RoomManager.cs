a39c52d16bdf44bc43ec109e80d6a14e571f6bbd
PiotrusWatson <piotruswatson@gmail.com>
2017-05-21 15:48:33 +0100

    made rooms nicer :)

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class RoomManager : MonoBehaviour {
 
 	public Team roomTeam = Team.neutral;
+	public int roomId = 0;
+	public int roomValue = 5;
 	// Use this for initialization
 	void Start () {
 		
